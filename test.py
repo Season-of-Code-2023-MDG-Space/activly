fa2dcf986480017fa2a2a8308b947005b65573f0
samarthjain422005 <87848220+samarthjain422005@users.noreply.github.com>
2023-03-13 10:36:59 +0530

    feat: added data processing + web hosting

---
+++ test.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask import request, jsonify
+
+import nltk 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+app = Flask(__name__)
+api = Api(app)
+
+
+def club_name_extracter(text):
+    print(14)
+    clubs=["PAG","SDS","MDG"]
+    no_of_words = text.split(' ')
+        
+    if len(no_of_words)<= 15:
+        print(len(no_of_words))
+        return
+
+    print(23)
+    words = nltk.word_tokenize(text)
+    words = [word for word in words if word not in set(stopwords.words('english'))]
+    tagged = nltk.pos_tag(words)
+    print(25)
+    for (word, tag) in tagged:
+
+        if tag == 'NNP':
+            if word in clubs:
+                print(text)
+                return text
+                
+
+
+class HelloWorld(Resource):
+    def get(self ):
+        data = request.get_json()
+        name = data['message']
+        t=club_name_extracter(name)
+        print(40)
+        return t
+
+
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
