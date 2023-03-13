5c421da8768fb52d17dca006e978f71e7b2596d7
ANGEL <mittalangel003@gmail.com>
2023-03-13 16:24:45 +0530

    feat:Notification Reader Implemented

--- Activly/app/src/main/java/com/example/activly/Clubsnotificationpage.kt
+++ Activly/app/src/main/java/com/example/activly/Clubsnotificationpage.kt
@@ -2,10 +2,25 @@ package com.example.activly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import com.MyListener
+import com.NotificationService
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
 
 class Clubsnotificationpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clubsnotificationpage)
+        }
     }
+}
+class Notifunc {
+    val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    override fun setValue(packageName: String?) {
+        packageName=myListener
+    }
+}
 }
